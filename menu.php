c58537024fc1c199b5c935bc0b122b7b54105c56
YutthapoomSa <wavee03@gmail.com>
2023-01-13 10:13:37 +0700

    🎨 IMPROVE:

--- menu.php
+++ menu.php
@@ -32,13 +32,15 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
+        <span>ข้อมูลสต็อก</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="blank.html">Dashboard Return</a>
-            <a class="collapse-item" href="stock-borrow.html">Dashboard Borrow</a>
+            <h6 class="collapse-header">ข้อมูลสต็อก:</h6>
+            <a class="collapse-item" href="stock-borrow.html">ใบยืม</a>
+            <a class="collapse-item" href="blank.html">ใบสั่งซื้อ</a>
+            <a class="collapse-item" href="stock-borrow.html">ยา</a>
+            <a class="collapse-item" href="stock-borrow.html">ใบคืน</a>
         </div>
     </div>
 </li>
@@ -48,16 +50,14 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
+        <span>ข้อมูลและสถิติ</span>
     </a>
     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <h6 class="collapse-header">ข้อมูลและสถิติ</h6>
+            <a class="collapse-item" href="utilities-color.html">จำนวนผู้รับบริการ</a>
+            <a class="collapse-item" href="utilities-border.html">ภาพรวมผู้ป่วยใน</a>
         </div>
     </div>
 </li>
